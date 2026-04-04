86cf0e95294288804d59a98711ee1f45adf81af3
Yudan Zhou <zhou.yud@northeastern.edu>
2026-04-03 22:00:01 -0400

    update

--- src/test/java/adt/MyBagTest.java
+++ src/test/java/adt/MyBagTest.java
@@ -1,11 +1,13 @@
 package adt;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.util.Iterator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class MyBagTest {
 
